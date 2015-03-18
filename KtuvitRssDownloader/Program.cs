c582b36a380b2dc752bb978195bf6dda36a90b85
MrLesh <alonmatat@gmail.com>
2015-03-18 18:19:05 +0200

    did a good login

--- KtuvitRssDownloader/Program.cs
+++ KtuvitRssDownloader/Program.cs
@@ -9,35 +9,34 @@ using System.Net;
 
 namespace KtuvitRssDownloader
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string url = @"http://rss.ktuvit.com/myseriesrss.php?uid=348679&ticket=AC8846E87";
-            string subtitleDownloadUrl = @"http://www.ktuvit.com/downloadsubtitle.php?id={0}";
-            XmlReader reader = XmlReader.Create(url);
-            RssFeed feed = RssFeed.Create(new Uri(url)); 
-            foreach (var item in feed.Channel.Items)
-            {
-
-                Console.WriteLine(string.Format("Name:{0}, Link:{1}, Date:{2}", item.Title, item.Link, item.PublicationDate));
-                if(item.HasExtensions)
-                {
-                    var content = item.Extensions.Where(i=>i.XmlPrefix=="content").First();
-                    var pos1 = content.ToString().IndexOf(@";m=subtitles#");
-                    var pos2 = content.ToString().IndexOf(@"""", pos1);
-                    var id = content.ToString().Substring(pos1 + @";m=subtitles#".Length, pos2 - pos1 - @";m=subtitles#".Length);
-                    Console.WriteLine(string.Format("ID of subtitle:{0}", id));
-                    Console.WriteLine("-----------------------");
-                    using(WebClient client = new WebClient())
-	                {
-                        var filename = item.Title.Substring(item.Title.IndexOf('|')+2);
-                        client.DownloadFile(string.Format(subtitleDownloadUrl, id), string.Format(@"D:\TV Shows\{0}.zip", filename));
-	                }
-                    
-                }
-                
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string url = @"http://rss.ktuvit.com/myseriesrss.php?uid=348679&ticket=AC8846E87";
+			var downloader = new SubtitleDownloader();
+			var feed = RssFeed.Create(new Uri(url)); 
+			foreach (var item in feed.Channel.Items)
+			{
+				Console.WriteLine("Name:{0}, Link:{1}, Date:{2}", item.Title, item.Link, item.PublicationDate);
+				if(item.HasExtensions)
+				{
+					var content = item.Extensions.First(i => i.XmlPrefix=="content");
+					var pos1 = content.ToString().IndexOf(@";m=subtitles#");
+					var pos2 = content.ToString().IndexOf(@"""", pos1);
+					var id = content.ToString().Substring(pos1 + @";m=subtitles#".Length, pos2 - pos1 - @";m=subtitles#".Length);
+					Console.WriteLine(string.Format("ID of subtitle:{0}", id));
+					Console.WriteLine("-----------------------");
+                    downloader.Download(id,item.Title.Substring(item.Title.IndexOf('|')+2));
+                    //using(var client = new WebClient())
+                    //{
+                    //    var filename = item.Title.Substring(item.Title.IndexOf('|')+2);
+                    //    client.DownloadFile(string.Format(subtitleDownloadUrl, id), string.Format(@"D:\TV Shows\{0}.zip", filename));
+                    //}
+					
+				}
+				
+			}
+		}
+	}
 }
