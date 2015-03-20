e9c44260f44a180a7294683f8ad13e1126b463c7
MrLesh <alonmatat@gmail.com>
2015-03-20 16:06:03 +0200

    Added the mian GUI

---
+++ KtuvitRssDownloaderGui/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace KtuvitRssDownloaderGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblForInterval = new System.Windows.Forms.Label();
+            this.btnDirectoryToSave = new System.Windows.Forms.Button();
+            this.txtBoxDirectoryToSave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFeedInfo = new System.Windows.Forms.Label();
+            this.chBoxShouldRunAtStartUp = new System.Windows.Forms.CheckBox();
+            this.checkRssInterval = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayIconContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.checkRssInterval)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.trayIconContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabs
+            // 
+            this.tabs.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Location = new System.Drawing.Point(0, 26);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(425, 298);
+            this.tabs.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnCancel);
+            this.tabPage1.Controls.Add(this.btnSave);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.lblForInterval);
+            this.tabPage1.Controls.Add(this.btnDirectoryToSave);
+            this.tabPage1.Controls.Add(this.txtBoxDirectoryToSave);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lblFeedInfo);
+            this.tabPage1.Controls.Add(this.chBoxShouldRunAtStartUp);
+            this.tabPage1.Controls.Add(this.checkRssInterval);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(417, 269);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "minutes";
+            // 
+            // lblForInterval
+            // 
+            this.lblForInterval.AutoSize = true;
+            this.lblForInterval.Location = new System.Drawing.Point(9, 131);
+            this.lblForInterval.Name = "lblForInterval";
+            this.lblForInterval.Size = new System.Drawing.Size(125, 13);
+            this.lblForInterval.TabIndex = 6;
+            this.lblForInterval.Text = "RSS feed refresh interval";
+            // 
+            // btnDirectoryToSave
+            // 
+            this.btnDirectoryToSave.Location = new System.Drawing.Point(375, 85);
+            this.btnDirectoryToSave.Name = "btnDirectoryToSave";
+            this.btnDirectoryToSave.Size = new System.Drawing.Size(36, 23);
+            this.btnDirectoryToSave.TabIndex = 5;
+            this.btnDirectoryToSave.Text = "...";
+            this.btnDirectoryToSave.UseVisualStyleBackColor = true;
+            this.btnDirectoryToSave.Click += new System.EventHandler(this.btnDirectoryToSave_Click);
+            // 
+            // txtBoxDirectoryToSave
+            // 
+            this.txtBoxDirectoryToSave.Location = new System.Drawing.Point(9, 86);
+            this.txtBoxDirectoryToSave.Name = "txtBoxDirectoryToSave";
+            this.txtBoxDirectoryToSave.Size = new System.Drawing.Size(360, 20);
+            this.txtBoxDirectoryToSave.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose where to save subtitles to:";
+            // 
+            // lblFeedInfo
+            // 
+            this.lblFeedInfo.AutoSize = true;
+            this.lblFeedInfo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lblFeedInfo.Location = new System.Drawing.Point(9, 17);
+            this.lblFeedInfo.Name = "lblFeedInfo";
+            this.lblFeedInfo.Size = new System.Drawing.Size(130, 13);
+            this.lblFeedInfo.TabIndex = 2;
+            this.lblFeedInfo.Text = "Paste you RSS feed here:";
+            // 
+            // chBoxShouldRunAtStartUp
+            // 
+            this.chBoxShouldRunAtStartUp.AutoSize = true;
+            this.chBoxShouldRunAtStartUp.Location = new System.Drawing.Point(9, 173);
+            this.chBoxShouldRunAtStartUp.Name = "chBoxShouldRunAtStartUp";
+            this.chBoxShouldRunAtStartUp.Size = new System.Drawing.Size(200, 17);
+            this.chBoxShouldRunAtStartUp.TabIndex = 2;
+            this.chBoxShouldRunAtStartUp.Text = "Start donwloader on windows startup";
+            this.chBoxShouldRunAtStartUp.UseVisualStyleBackColor = true;
+            this.chBoxShouldRunAtStartUp.CheckedChanged += new System.EventHandler(this.chBoxShouldRunAtStartUp_CheckedChanged);
+            // 
+            // checkRssInterval
+            // 
+            this.checkRssInterval.Location = new System.Drawing.Point(140, 127);
+            this.checkRssInterval.Name = "checkRssInterval";
+            this.checkRssInterval.Size = new System.Drawing.Size(52, 20);
+            this.checkRssInterval.TabIndex = 1;
+            this.checkRssInterval.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(417, 269);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Logs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(414, 295);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(423, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(336, 235);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(246, 235);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.trayIconContextMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // trayIconContextMenu
+            // 
+            this.trayIconContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restoreToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem1});
+            this.trayIconContextMenu.Name = "trayIconContextMenu";
+            this.trayIconContextMenu.Size = new System.Drawing.Size(114, 54);
+            this.trayIconContextMenu.Text = "Ktuvit RSS downloader";
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restoreToolStripMenuItem.Text = "Restore";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(423, 321);
+            this.Controls.Add(this.tabs);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ktuvit RSS downloader";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.checkRssInterval)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.trayIconContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnDirectoryToSave;
+        private System.Windows.Forms.TextBox txtBoxDirectoryToSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFeedInfo;
+        private System.Windows.Forms.CheckBox chBoxShouldRunAtStartUp;
+        private System.Windows.Forms.NumericUpDown checkRssInterval;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblForInterval;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip trayIconContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+    }
+}
+
---
+++ KtuvitRssDownloaderGui/Form1.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KtuvitRssDownloaderGui
+{
+    public partial class Form1 : Form
+    {
+        private const string AppName = "Ktuvit RSS downloader";
+        // The path to the key where Windows looks for startup applications
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        public Form1()
+        {
+            InitializeComponent();
+            // Check to see the current state (running at startup or not)
+            CheckRegistryIfRunAtStartup();
+        }
+
+        public void frmStartup()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckRegistryIfRunAtStartup()
+        {
+            if (rkApp.GetValue(AppName) == null)
+            {
+                // The value doesn't exist, the application is not set to run at startup
+                chBoxShouldRunAtStartUp.Checked = false;
+            }
+            else
+            {
+                // The value exists, the application is set to run at startup
+                chBoxShouldRunAtStartUp.Checked = true;
+            }
+        }
+        
+
+        private void btnDirectoryToSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxDirectoryToSave.Text = folderBrowserDialog1.SelectedPath;
+               
+            }
+        }
+
+        private void chBoxShouldRunAtStartUp_CheckedChanged(object sender, System.EventArgs e)
+        {
+            if (chBoxShouldRunAtStartUp.Checked)
+            {
+                // Add the value in the registry so that the application runs at startup
+                rkApp.SetValue(AppName, Application.ExecutablePath.ToString());
+            }
+            else
+            {
+                // Remove the value from the registry so that the application doesn't start
+                rkApp.DeleteValue(AppName, false);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                MinimizeToTray();
+            }
+            //else if (this.WindowState == FormWindowState.Normal)
+            //{
+            //    Restore();
+            //}
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            MinimizeToTray();
+        }
+
+        private void MinimizeToTray()
+        {
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Restore();
+        }
+
+        private void Restore()
+        {
+            notifyIcon1.Visible = false;
+            this.Show();
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            // Activate the form. 
+
+            this.Activate();
+        }
+
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Restore();
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //launch thread to start downloading
+        }
+
+    }
+}
